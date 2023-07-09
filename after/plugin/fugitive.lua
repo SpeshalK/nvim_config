d4b8639ec5b3dec1f5d2deef8bc39f0bf00ece93
Tomas Kozlej <kozlej.tomas@strath.ac.uk>
2023-07-09 12:51:09 +0100

    Adding a git push key binding: <leader>gp

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n","<leader>gg",vim.cmd.Git);
 vim.keymap.set("n","<leader>gd",":Gdiffsplit<CR>");
+vim.keymap.set("n","<leader>gp",":Git push<CR>");
 vim.keymap.set("n","<leader>gits",":Git status<CR>");
